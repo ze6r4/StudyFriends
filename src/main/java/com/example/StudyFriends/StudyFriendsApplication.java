1fda08c340d19adaa63274e5d166f5e1e012f0d6
ze6r4 <elenacerenkova603@gmail.com>
2025-11-12 16:41:19 +0400

    first commit

---
+++ src/main/java/com/example/StudyFriends/StudyFriendsApplication.java
@@ -0,0 +1,12 @@
+package com.example.StudyFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyFriendsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/StudyFriends/StudyFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudyFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

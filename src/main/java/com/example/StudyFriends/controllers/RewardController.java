0b134d0c43eec73a4f1671922c3bd76161412456
ze6r4 <elenacerenkova603@gmail.com>
2026-04-12 14:13:24 +0400

    ☕ Метод получения "полоски уровней"

--- src/main/java/com/example/StudyFriends/controllers/RewardController.java
+++ src/main/java/com/example/StudyFriends/controllers/RewardController.java
@@ -1,15 +1,13 @@
 package com.example.StudyFriends.controllers;
 
+import com.example.StudyFriends.config.RewardConfig;
 import com.example.StudyFriends.dto.SessionDto;
 import com.example.StudyFriends.dto.reward.SessionReward;
 import com.example.StudyFriends.services.RewardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/rewards")
@@ -30,5 +28,9 @@ public class RewardController {
                     .status(HttpStatus.BAD_REQUEST)
                     .body("Ошибка: " + ex.getMessage());
         }
+    }
+    @GetMapping("/skill/levels")
+    public ResponseEntity<?> getSkillStages() {
+        return ResponseEntity.ok(RewardConfig.SKILL_STAGES);
     }
 }
